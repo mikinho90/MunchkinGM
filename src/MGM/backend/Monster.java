5cbbb970be7f21d1efdb5bcc19af5dd0c48eec0f
gitHen <hendrik.janiesch+github@gmail.com>
2014-07-10 02:18:36 +0200

    + player implementation + monster implementation started

--- src/MGM/backend/Monster.java
+++ src/MGM/backend/Monster.java
@@ -1,9 +1,31 @@
 package MGM.backend;
 
 public class Monster {
+	
+	private Integer power;
+	private Integer loot;
+	private Integer xp;
 
 	public Monster() {
-		// TODO Auto-generated constructor stub
+		resetMonster();
 	}
+
+	public Integer getPower() {
+		return power;
+	}
+
+	public Integer getLoot() {
+		return loot;
+	}
+
+	public Integer getXp() {
+		return xp;
+	}
+	
+	public void resetMonster(){
+		this.power = 0;
+		this.loot = 0;
+		this.xp = 0;
+	}	
 
 }
